c5e36afe2a9c07bcba1b2832b9b1a27f3061632e
Ewerton Melo <developerewt@gmail.com>
2016-07-25 10:58:33 -0300

    First commit

---
+++ webix.i18n.pt-BR.js
@@ -0,0 +1,36 @@
+webix.i18n.locales["pt-BR"]={  
+                groupDelimiter:".",       
+                groupSize:3,              
+                decimalDelimiter:",",     
+                decimalSize:2,            
+                fileSize: ["b","Kb","Mb","Gb","Tb","Pb","Eb"],
+                dateFormat:"%d/%m/%Y",
+                timeFormat:"%H:%i",
+                longDateFormat:"%d %F %Y",
+                fullDateFormat:"%d.%m.%Y %H:%i",
+                am:null,
+                pm:null,
+                price:"R$ {obj}",
+                calendar:{
+                    monthFull:["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto",
+                        "Setembro", "Outubro", "Novembro", "Dezembro"],
+                    monthShort:["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out",
+                        "Nov", "Dez"],
+                    dayFull:["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"],
+                    dayShort:["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"]
+                },
+                hours: "Horas ",
+                minutes: "Minutos",
+                done:"Concluído",
+                clear: "Limpar",
+                today: "Hoje"
+
+                controls:{
+                    select:"Selecione"
+                }
+
+                dataExport:{
+                    page:"Página",
+                    of:"de"
+                }
+            };
